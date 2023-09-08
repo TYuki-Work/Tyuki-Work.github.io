b3740ca2211d33a5713d61546b8742a5b0716f91
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-08 18:04:02 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,33 +8,41 @@
  // Initialize the Firebase app in the service worker by passing in
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
- firebase.initializeApp({
-    apiKey: 'AIzaSyDNtLQ9ZbAtRj1IrN8XyP-y5jH9L5aYu0w',
-    authDomain: 'pushtest01-ded5f.firebaseapp.com',
-    databaseURL: 'https://pushtest01-ded5f.firebaseio.com',
-    projectId: 'pushtest01-ded5f',
-    storageBucket: 'pushtest01-ded5f.appspot.com',
-    messagingSenderId: '71015566445',
-    appId: '1:71015566445:web:3e17509a6268d959124364',
-    measurementId: 'G-0CLQF7L3E2',
- });
+firebase.initializeApp({
+  apiKey: 'AIzaSyDNtLQ9ZbAtRj1IrN8XyP-y5jH9L5aYu0w',
+  authDomain: 'pushtest01-ded5f.firebaseapp.com',
+  databaseURL: 'https://pushtest01-ded5f.firebaseio.com',
+  projectId: 'pushtest01-ded5f',
+  storageBucket: 'pushtest01-ded5f.appspot.com',
+  messagingSenderId: '71015566445',
+  appId: '1:71015566445:web:3e17509a6268d959124364',
+  measurementId: 'G-0CLQF7L3E2',
+});
 
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
- const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
 
 messaging.onMessage((payload) => {
   console.log('Message received. ', payload);
-  // ...
+  self.addEventListener('push', function (event) {
+    const title = 'プッシュ通知のテスト';
+    const options = {
+      body: event.data.text(), // サーバーからのメッセージ
+      tag: title, // タイトル
+      icon: 'icon.png', // アイコン
+      badge: 'icon.png' // アイコン
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
+
+  // プッシュ通知をクリックしたときのイベント
 });
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// Keep in mind that FCM will still show notification messages automatically 
-// and you should use data messages for custom notifications.
-// For more info see: 
-// https://firebase.google.com/docs/cloud-messaging/concept-options
+
+
+
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
@@ -45,5 +53,16 @@ messaging.onBackgroundMessage(function(payload) {
   };
 
   self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  notificationOptions);
+});
+
+
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
+    clients.openWindow('index.html')
+  );
 });
