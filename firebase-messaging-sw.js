df63b04824d60d5abed547e6d5cbabe4b62fb845
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-19 14:39:24 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
 
-// self.addEventListener('push', function (event) {
-//   const title = 'プッシュ通知のテスト';
-//   const options = {
-//     body: event.data.text(), // サーバーからのメッセージ
-//     tag: title, // タイトル
-//     icon: '/firebase-logo.png', // アイコン
-//     badge: '/firebase-logo.png' // アイコン
-//   };
+self.addEventListener('push', function (event) {
+  const title = 'プッシュ通知のテスト';
+  const options = {
+    body: event.data.text(), // サーバーからのメッセージ
+    tag: title, // タイトル
+    icon: '/firebase-logo.png', // アイコン
+    badge: '/firebase-logo.png' // アイコン
+  };
 
-//   event.waitUntil(self.registration.showNotification(title, options));
-// });
+  event.waitUntil(self.registration.showNotification(title, options));
+});
 
 
-// self.addEventListener('notificationclick', function (event) {
-//   event.notification.close();
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
 
-//   event.waitUntil(
-//     // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
-//     clients.openWindow('/index.html')
-//   );
-// });
+  event.waitUntil(
+    // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
+    clients.openWindow('/index.html')
+  );
+});
