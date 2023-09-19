ca0fa4bee7c353c493d44d3d9e38ba97723151f5
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-19 17:39:50 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,26 +2,26 @@
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here. Other Firebase libraries
  // are not available in the service worker.
- importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
- importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
+//  importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
+//  importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
 
  // Initialize the Firebase app in the service worker by passing in
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: 'AIzaSyDNtLQ9ZbAtRj1IrN8XyP-y5jH9L5aYu0w',
-  authDomain: 'pushtest01-ded5f.firebaseapp.com',
-  databaseURL: 'https://pushtest01-ded5f.firebaseio.com',
-  projectId: 'pushtest01-ded5f',
-  storageBucket: 'pushtest01-ded5f.appspot.com',
-  messagingSenderId: '71015566445',
-  appId: '1:71015566445:web:3e17509a6268d959124364',
-  measurementId: 'G-0CLQF7L3E2',
-});
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyDNtLQ9ZbAtRj1IrN8XyP-y5jH9L5aYu0w',
+//   authDomain: 'pushtest01-ded5f.firebaseapp.com',
+//   databaseURL: 'https://pushtest01-ded5f.firebaseio.com',
+//   projectId: 'pushtest01-ded5f',
+//   storageBucket: 'pushtest01-ded5f.appspot.com',
+//   messagingSenderId: '71015566445',
+//   appId: '1:71015566445:web:3e17509a6268d959124364',
+//   measurementId: 'G-0CLQF7L3E2',
+// });
 
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 // self.addEventListener('push', function (event) {
 //   const title = 'プッシュ通知のテスト';
@@ -61,11 +61,11 @@ const messaging = firebase.messaging();
 // });
 
 
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
+// self.addEventListener('notificationclick', function (event) {
+//   event.notification.close();
 
-  event.waitUntil(
-    // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
-    clients.openWindow('/index.html')
-  );
-});
+//   event.waitUntil(
+//     // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
+//     clients.openWindow('/index.html')
+//   );
+// });
