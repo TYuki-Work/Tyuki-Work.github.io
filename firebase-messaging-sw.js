484b5483ba69976f049db86f56f0efa91a168cae
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-19 14:22:47 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,63 +1,14 @@
 
- // Give the service worker access to Firebase Messaging.
- // Note that you can only use Firebase Messaging here. Other Firebase libraries
- // are not available in the service worker.
- importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
- importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
-
- // Initialize the Firebase app in the service worker by passing in
- // your app's Firebase config object.
- // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: 'AIzaSyDNtLQ9ZbAtRj1IrN8XyP-y5jH9L5aYu0w',
-  authDomain: 'pushtest01-ded5f.firebaseapp.com',
-  databaseURL: 'https://pushtest01-ded5f.firebaseio.com',
-  projectId: 'pushtest01-ded5f',
-  storageBucket: 'pushtest01-ded5f.appspot.com',
-  messagingSenderId: '71015566445',
-  appId: '1:71015566445:web:3e17509a6268d959124364',
-  measurementId: 'G-0CLQF7L3E2',
-});
-
- // Retrieve an instance of Firebase Messaging so that it can handle background
- // messages.
-const messaging = firebase.messaging();
-
-// self.addEventListener('push', function (event) {
-//   const title = 'プッシュ通知のテスト';
-//   const options = {
-//     body: event.data.text(), // サーバーからのメッセージ
-//     tag: title, // タイトル
-//     icon: '/firebase-logo.png', // アイコン
-//     badge: '/firebase-logo.png' // アイコン
-//   };
-
-//   event.waitUntil(self.registration.showNotification(title, options));
-// });
-
-
-// messaging.onMessage(function(payload){
-//   const title = 'プッシュ通知のテスト';
-//   const options = {
-//     body: event.data.text(), // サーバーからのメッセージ
-//     tag: title, // タイトル
-//     icon: '/firebase-logo.png', // アイコン
-//     badge: '/firebase-logo.png' // アイコン
-//   };
-//   self.registration.showNotification(title, options);
-// });
-
-
-
-
-messaging.onBackgroundMessage(function(payload){
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message',
-    icon: '/firebase-logo.png'
+self.addEventListener('push', function (event) {
+  const title = 'プッシュ通知のテスト';
+  const options = {
+    body: event.data.text(), // サーバーからのメッセージ
+    tag: title, // タイトル
+    icon: '/firebase-logo.png', // アイコン
+    badge: '/firebase-logo.png' // アイコン
   };
-  self.registration.showNotification(notificationTitle,notificationOptions);
+
+  event.waitUntil(self.registration.showNotification(title, options));
 });
 
 
