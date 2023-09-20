095ba77d2369b01b58476d2020713741a284fd5f
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-20 11:23:02 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -49,17 +49,17 @@
 // });
 
 
-self.addEventListener('push', function (event) {
-  const title = 'プッシュ通知のテスト';
-  const options = {
-    body: event.data.text(), // サーバーからのメッセージ
-    tag: title, // タイトル
-    icon: '/firebase-logo.png', // アイコン
-    badge: '/firebase-logo.png' // アイコン
-  };
+// self.addEventListener('push', function (event) {
+//   const title = 'プッシュ通知のテスト';
+//   const options = {
+//     body: event.data.text(), // サーバーからのメッセージ
+//     tag: title, // タイトル
+//     icon: '/firebase-logo.png', // アイコン
+//     badge: '/firebase-logo.png' // アイコン
+//   };
 
-  event.waitUntil(self.registration.showNotification(title, options));
-});
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
 
 
 // self.addEventListener('notificationclick', function (event) {
