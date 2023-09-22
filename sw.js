44eebf31fbcb42747f3f8bd0a834eff82b42254f
TYuki-Work <143775843+TYuki-Work@users.noreply.github.com>
2023-09-22 11:45:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+
+self.addEventListener('install', () => {
+  console.log("installed");
+});
+
+
+self.addEventListener('activate', (e) => {
+  console.log("activated");
+  e.waitUntil(self.clients.claim());
+});
+
+
+self.addEventListener('push', function (event) {
+  const title = 'プッシュ通知のテスト';
+  const options = {
+    body: event.data.text(), // サーバーからのメッセージ
+    tag: title, // タイトル
+    icon: '/firebase-logo.png', // アイコン
+    badge: '/firebase-logo.png' // アイコン
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    // プッシュ通知をクリックしたときにブラウザを起動して表示するURL
+    clients.openWindow('/index.html')
+  );
+});
